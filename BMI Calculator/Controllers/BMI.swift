5c0d13cc9bae1aca5f36b73f7a73a19a571f63f5
Diyas Agmanov <62609041+diyas-ui@users.noreply.github.com>
2021-03-30 19:35:10 +0600

    Udemy Apps

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by mac on 4/17/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
