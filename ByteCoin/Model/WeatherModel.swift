6eea10905bd38c4ae9e1029c054bc7b33512ed79
Diyas Agmanov <62609041+diyas-ui@users.noreply.github.com>
2021-03-30 19:44:11 +0600

    Json-ByteCoin13

---
+++ ByteCoin/Model/WeatherModel.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherModel.swift
+//  ByteCoin
+//
+//  Created by mac on 3/4/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct WeatherModel {
+    var rate: Double
+    var currency : String
+}
